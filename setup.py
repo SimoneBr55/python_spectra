da2fffc65d5671fdb09beab2d88b95cf5002ed81
SimoneBr55 <39730275+SimoneBr55@users.noreply.github.com>
2022-05-07 12:30:50 +0200

    Given a shape

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setupsetup(
+    name='spectroscopy',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Python Library for spectral reduction and analysis.',
+    author='Simone Brazioli',
+    license='MIT',
+    install_requires['astropy']
+)
